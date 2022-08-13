8fe6cb8b2d262fdd542ec26baed05b3465db7738
KISLY74 <kisly.nikita@mail.ru>
2022-08-13 18:10:02 +0300

    feat: add routes for check and getUsers methods

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController')
 
 router.post('/regin', userController.regin)
 router.post('/login', userController.login)
+router.get('/auth', userController.check)
+router.get('/get/all', userController.getUsers)
 
 module.exports = router
