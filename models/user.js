88b0aa5cea9000d6301cab37dd1181f749483f7f
KISLY74 <kisly.nikita@mail.ru>
2022-08-13 18:22:20 +0300

    fix: change field role on roles because user can be user and admin

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const User = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   username: { type: String, required: true },
-  role: { type: String, ref: 'Role', required: true, default: 'User' }
+  roles: [{ type: String, ref: 'Role' }]
 }, { versionKey: false })
 
 module.exports = new model('User', User)
