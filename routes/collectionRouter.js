1995fb4f2af46e43e881192ef853f7535ceaadd7
KISLY74 <kisly.nikita@mail.ru>
2022-08-23 22:24:52 +0300

    feat: import class for static method, and create routers for methods of getting collections by ids from user and getting collections by user (this method get collections of user with method getcollectionsbyids, which get ids from user array of collections)

--- routes/collectionRouter.js
+++ routes/collectionRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express')
 const router = new Router()
-const collectionController = require('../controllers/collectionController')
+const { CollectionController, collectionController } = require('../controllers/collectionController')
 
 router.post('/create', collectionController.createCollection)
 router.get('/get/all', collectionController.getCollections)
+router.post('/getAllByUser', collectionController.getCollectionsByUser)
+router.post('/getByIds', CollectionController.getCollectionsByIds)
 
 module.exports = router
