7e3249278337f4d011d7c618e83c7645d00782d5
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 17:17:28 +0300

    feat: add reference to field items model item

--- models/collection.js
+++ models/collection.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('mongoose')
 const Collection = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  theme: { type: String, required: true }
+  theme: { type: String, required: true },
+  items: [{ type: String, ref: 'Item' }]
 }, { versionKey: false })
 
 module.exports = new model('Collection', Collection)
