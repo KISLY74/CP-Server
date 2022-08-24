40836273a529009b65df43666e0807d46120d742
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 13:45:07 +0300

    feat: create route for method delete collections of user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -12,5 +12,6 @@ router.put('/change/status', userController.changeStatus)
 router.put('/delete', userController.deleteUser)
 router.put('/add', userController.addToAdmins)
 router.put('/remove', userController.removeFromAdmins)
+router.put('/change/collections', userController.changeCollectionsUser)
 
 module.exports = router
