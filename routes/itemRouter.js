09606779a30647a3fc4903e55a93a071c3b86a52
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 17:34:57 +0300

    feat: create routes for methods of get ids items and get items by collection

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const router = new Router()
-const itemController = require('../controllers/itemController')
+const { itemController, ItemController } = require('../controllers/itemController')
 
 router.post('/create', itemController.createItem)
 router.put('/delete', itemController.deleteItem)
 router.get('/get/all', itemController.getAllItems)
+router.post('/getAllByCollection', itemController.getItemsByCollection)
+router.post('/getAllByIds', ItemController.getItemsByIds)
 
 module.exports = router
