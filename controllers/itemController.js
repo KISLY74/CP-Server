f19f23ab3b308cfe0feca354c539db284ccf92db
KISLY74 <kisly.nikita@mail.ru>
2022-08-27 21:04:25 +0300

    feat: implementate addition more fields

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,21 +1,29 @@
 const Collection = require("../models/collection")
 const User = require("../models/user")
-const Item = require("../models/item")
+const { ItemM, Item } = require("../models/item")
 
 class ItemController {
   async createItem(req, res) {
-    const { name, tags, id } = req.body
-    const item = new Item({ name: name, tags: tags, dateAddition: Date.now() })
+    const { name, tags, id, additionalFields } = req.body
+    let fieldsSchema = {}, fieldsCreate = {}, types = ["string", "number", "boolean", "date"]
+    for (let i = 0; i < additionalFields.length; i++) {
+      for (let key in additionalFields[i]) {
+        fieldsSchema[key] = types[i]
+        fieldsCreate[key] = additionalFields[i][key]
+      }
+    }
+    Item.add(fieldsSchema)
+    const item = new ItemM({ name: name, tags: tags, dateAddition: Date.now(), ...fieldsCreate })
     item.save()
     await Collection.findOneAndUpdate({ _id: id }, { $push: { items: item._id } })
     return res.json(item)
   }
   async deleteItem(req, res) {
-    const result = await Item.deleteOne({ _id: req.body.id })
+    const result = await ItemM.deleteOne({ _id: req.body.id })
     return res.json(result)
   }
   async getAllItems(req, res) {
-    const items = await Item.find()
+    const items = await ItemM.find()
     return res.json(items)
   }
   async getItemsByCollection(req, res) {
@@ -24,11 +32,11 @@ class ItemController {
     return res.json(items)
   }
   static async getItemsByIds(ids) {
-    const items = await Item.find({ _id: { $in: ids } })
+    const items = await ItemM.find({ _id: { $in: ids } })
     return items
   }
   async editItem(req, res) {
-    const item = await Item.findOneAndUpdate({ _id: req.body.id }, { name: req.body.name, tags: req.body.tags })
+    const item = await ItemM.findOneAndUpdate({ _id: req.body.id }, { name: req.body.name, tags: req.body.tags })
     return res.json(item)
   }
 }
