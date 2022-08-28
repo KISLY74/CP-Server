1313bc8a25e4b9f7d3aefeb7b4975152a5a76e1a
KISLY74 <kisly.nikita@mail.ru>
2022-08-28 23:04:56 +0300

    feat: add fields for additional fields to items of them collection

--- models/collection.js
+++ models/collection.js
@@ -4,7 +4,11 @@ const Collection = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   theme: { type: String, required: true },
-  items: [{ type: String, ref: 'Item' }]
+  items: [{ type: String, ref: 'Item' }],
+  stringsFields: [{ type: String }],
+  numbersFields: [{ type: String }],
+  bolleansFields: [{ type: String }],
+  datesFields: [{ type: String }]
 }, { versionKey: false })
 
 module.exports = new model('Collection', Collection)
