c050e14c81aab7c36d3c36937dc7364b05f2c6eb
KISLY74 <kisly.nikita@mail.ru>
2022-08-29 16:53:44 +0300

    feat: add route to method of get item

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -8,5 +8,6 @@ router.get('/get/all', itemController.getAllItems)
 router.post('/getAllByCollection', itemController.getItemsByCollection)
 router.post('/getAllByIds', ItemController.getItemsByIds)
 router.put('/edit', itemController.editItem)
+router.post('/get', itemController.getItem)
 
 module.exports = router
